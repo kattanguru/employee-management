558b2f9e19b26f23031ba242c54e7eac5dc7f4b9
Nagaraju Kattanguru <Nagaraju.Kattanguru@albertsons.com>
2022-07-31 21:21:26 +0530

    Security updates

--- src/main/java/com/greatlearning/employee/config/WebSecurityConfig.java
+++ src/main/java/com/greatlearning/employee/config/WebSecurityConfig.java
@@ -1,5 +1,9 @@
 package com.greatlearning.employee.config;
 
+import com.greatlearning.employee.entity.Role;
+import com.greatlearning.employee.repository.RoleRepository;
+import com.greatlearning.employee.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,14 +14,25 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public static final String ADMIN = "ADMIN";
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/employees").authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
@@ -30,12 +45,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("password")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
+        List<com.greatlearning.employee.entity.User> users = userRepository.findAll();
+        List<UserDetails> userDetails = new ArrayList<>();
+        users.forEach(normalUser -> {
+            List<Role> role = roleRepository.findByRoleId(String.valueOf(normalUser.getRoleId()));
+            role.forEach(singleRole -> {
+                if (ADMIN.equalsIgnoreCase(singleRole.getName())) {
+                    UserDetails user = User.withDefaultPasswordEncoder()
+                            .username(normalUser.getUserName())
+                            .password(normalUser.getPassword())
+                            .roles(singleRole.getName())
+                            .build();
+                    userDetails.add(user);
+                }
+            });
+        });
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
--- src/main/java/com/greatlearning/employee/repository/RoleRepository.java
+++ src/main/java/com/greatlearning/employee/repository/RoleRepository.java
@@ -4,10 +4,11 @@ import com.greatlearning.employee.entity.Role;
 import com.greatlearning.employee.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Optional<User> findByRoleId(String roleId);
+    List<Role> findByRoleId(String roleId);
 
-    Optional<User> findByName(String name);
+    Optional<Role> findByName(String name);
 }
